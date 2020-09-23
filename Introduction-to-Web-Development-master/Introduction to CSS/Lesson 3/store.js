cd34d9fc27a00c4bbcb6bedd91b8c8ad92874f62
yongqin3759 <yongqin3759@gmail.com>
2020-09-23 10:17:53 +0800

    35 mins in

--- Introduction-to-Web-Development-master/Introduction to CSS/Lesson 3/store.js
+++ Introduction-to-Web-Development-master/Introduction to CSS/Lesson 3/store.js
@@ -9,13 +9,17 @@ var removeCartItemButtons = document.getElementsByClassName('btn-danger')
         var button = removeCartItemButtons[i]
         button.addEventListener('click', function(event){
             removeCartItem(event);
-            console.log('i am here');
     })
     }
     var quantityInputs = document.getElementsByClassName('cart-quantity-input')
     for (var i = 0; i < quantityInputs.length; i++){
         var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
+    }
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++){
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
     }
 }
 
@@ -33,6 +37,42 @@ function quantityChanged(event){
     updateCartTotal()
 }
 
+function addToCartClicked(event){
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    console.log(title, price, imageSrc)
+    addItemToCart(title, price, imageSrc)
+}
+
+function addItemToCart(title, price, imageSrc){
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    //create div to later add to html
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for(i=0; i< cartItemNames.length;i++){
+        if (cartItemNames[i].innerText = title){
+            alert('This item has already been added')
+            return
+            //call return to stop executing function
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src= "${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+}
 
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
